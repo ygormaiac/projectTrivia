860cf5366cb90187a110901abf62f9dff29ebf29
Ygor <ygormaiaa1@gmail.com>
2021-09-03 18:28:11 -0300

    Requisito 04 ok

--- src/pages/Play.js
+++ src/pages/Play.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import md5 from 'crypto-js/md5';
 
 class Play extends Component {
+  constructor() {
+    super();
+
+    this.emailUser = this.emailUser.bind(this);
+  }
+
+  emailUser() {
+    const { emailPlayer } = this.props;
+    return md5(emailPlayer).toString();
+  }
+
   render() {
+    const { namePlayer } = this.props;
+
     return (
-      <div>Play the game!</div>
+      <div>
+        <header data-testid="header-player-name">
+          <img 
+          data-testid="header-profile-picture" 
+          alt="imageUser" 
+          src={`https://www.gravatar.com/avatar/${ this.emailUser() }`}
+          />
+          <span>{ namePlayer }</span>
+          <span data-testid="header-score">0</span>
+        </header>
+      </div>
     );
   }
 }
 
-export default Play;
+const mapStateToProps = (state) => ({
+  namePlayer: state.userReducer.name,
+  emailPlayer: state.userReducer.email,
+})
+
+export default connect(mapStateToProps)(Play);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,6 +1,48 @@
 export const SET_PERSONAL_INFO = 'SET_PERSONAL_INFO';
+export const LOADING_GAME = 'LOADING_API';
+export const SUCCESS_INITIAL_GAME = 'SUCCESS_INITIAL_GAME';
+export const ERROR_INITIAL_GAME = 'ERROR_INITIAL_GAME';
 
 export const saveLoginInfo = (payload) => ({
   type: SET_PERSONAL_INFO,
   payload,
 });
+
+export const loadingGame = (payload) => ({
+  type: LOADING_GAME,
+  payload,
+});
+
+export const successGame = (payload) => ({
+  type: SUCCESS_INITIAL_GAME,
+  payload,
+});
+
+export const errorGame = (error) => ({
+  type: ERROR_INITIAL_GAME,
+  error,
+});
+
+// export const fetchGameAPI = (dispatch) => {
+//   dispatch(loadingGame());
+//   fetch(`https://opentdb.com/api.php?amount=5&token=${token}`)
+//     .then((response) => response.json()
+//       .then((data) => {
+//         dispatch(successGame(data)),
+//         dispatch(errorGame())
+//       }
+//     ));
+// }
+
+export function fetchGameAPI() {
+  return async (dispatch) => {
+    const token = JSON.parse(localStorage.getItem('token'));
+    dispatch(loadingGame());
+    try {
+      const data = await (await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`)).json();
+      return dispatch(successGame(data));
+    } catch (error) {
+      return dispatch(errorGame(error))
+    }
+  }
+}
