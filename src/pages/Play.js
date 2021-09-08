9b09497c06a90b0099fd2c66ebf578f221e0fa0f
Luiza Antiques <luiza.antiques@gmail.com>
2021-09-08 16:37:16 -0300

    Conclui requisito 5 e 6

--- src/pages/Play.js
+++ src/pages/Play.js
@@ -2,18 +2,34 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import Card from '../components/Card';
 import { fetchGameAPI } from '../redux/actions';
 
 class Play extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      indexQuestion: 0,
+    };
+  }
+
   componentDidMount() {
     const { resultAPI } = this.props;
     resultAPI();
   }
 
   render() {
+    const { results, loading } = this.props;
+    const { indexQuestion } = this.state;
+
+    if (loading === true) return <h2>Loading...</h2>;
+    console.log(results);
+    console.log(results[indexQuestion]);
     return (
       <div>
         <Header />
+        <Card question={ results[indexQuestion] } />
       </div>
     );
   }
@@ -21,10 +37,13 @@ class Play extends Component {
 
 Play.propTypes = {
   resultAPI: PropTypes.func.isRequired,
+  results: PropTypes.arrayOf(PropTypes.object).isRequired,
+  loading: PropTypes.bool.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   results: state.playerReducer.results,
+  loading: state.playerReducer.loading,
 });
 
 const mapDispatchToProps = (dispatch) => ({
