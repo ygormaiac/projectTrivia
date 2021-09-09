2be4a5508254277d03590091229ea1519491cfa7
Luiza Antiques <luiza.antiques@gmail.com>
2021-09-08 21:29:24 -0300

    Conclui requisito 8

--- src/pages/Play.js
+++ src/pages/Play.js
@@ -12,23 +12,55 @@ class Play extends Component {
 
     this.state = {
       indexQuestion: 0,
+      countdown: 30,
     };
+
+    this.countdown = this.countdown.bind(this);
+    this.timeOut = this.timeOut.bind(this);
   }
 
   componentDidMount() {
     const { resultAPI } = this.props;
     resultAPI();
+    this.countdown();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const MIN_SECONDS = 0;
+    if (prevState.countdown === MIN_SECONDS) {
+      this.timeOut();
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.cronometerInterval);
+  }
+
+  timeOut() {
+    document.querySelectorAll('.btn-answer')
+      .forEach((btn) => (btn.disabled === true));
+    this.setState({ countdown: 'Seu tempo acabou! :(' });
+    clearInterval(this.cronometerInterval);
+  }
+
+  countdown() {
+    const ONE_SECOND = 1000;
+
+    this.cronometerInterval = setInterval(() => {
+      this.setState((prevState) => ({ countdown: prevState.countdown - 1 }));
+    }, ONE_SECOND);
   }
 
   render() {
     const { results, loading } = this.props;
-    const { indexQuestion } = this.state;
+    const { indexQuestion, countdown } = this.state;
 
     if (loading === true) return <h2>Loading...</h2>;
     return (
       <div>
         <Header />
         <Card question={ results[indexQuestion] } />
+        <span>{countdown}</span>
       </div>
     );
   }
