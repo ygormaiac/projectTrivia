56c3907402422feafaf1e28cb26d73bcd5de274a
ygormaiac <ygormaiac@users.noreply.github.com>
2021-09-15 14:32:52 +0000

    deploy: 6f3b3ae05f45f855139a2cbb8b417b65193aa621

---
+++ precache-manifest.d3807de90ce79acbe99d27a7db571145.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffe959ff8a0eff68d3bb54c2bc0bee29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19d6a56afac8469c03e2",
+    "url": "/static/css/main.86446950.chunk.css"
+  },
+  {
+    "revision": "01840f9294b4587a0eab",
+    "url": "/static/js/2.8839788c.chunk.js"
+  },
+  {
+    "revision": "5d1b8067085602e259fd0862f57df793",
+    "url": "/static/js/2.8839788c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "19d6a56afac8469c03e2",
+    "url": "/static/js/main.224f1f50.chunk.js"
+  },
+  {
+    "revision": "0f6ff4c0bace0555b202",
+    "url": "/static/js/runtime-main.0aacbbf5.js"
+  },
+  {
+    "revision": "083fa82de3c99c4142396a32325f8a82",
+    "url": "/static/media/trivia-back.083fa82d.jpeg"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/static/media/trivia.466d153e.png"
+  }
+]);
